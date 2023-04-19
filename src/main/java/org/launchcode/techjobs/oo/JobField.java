377674b4079a2aef9b820281a4dc477213d99745
Jake Conwell <jakelconwell@gmail.com>
2023-04-19 14:14:14 -0500

    Finished assignment with all test passing and bonus on #5

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,46 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField(String value) {
+        id = nextId++;
+        this.value = value;
+        nextId++;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
